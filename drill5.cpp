1dfb00731bf3fd33fc92765603e3cb6437d6c572
jancsosz <55577212+jancsosz@users.noreply.github.com>
2019-10-17 20:43:52 +0200

    Add files via upload

---
+++ drill5.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(){
+    //1.
+    cout << "Success!\n";
+    //2.
+    cout << "Success!\n
+    //3.
+    cout << "Success! \n";
+    //4.
+    cout << "success! \n" << endl;
+    //5.
+    int res = 7; vector<int> v(10); v[51]= res; cout<<"Success!\n";
+    //6.
+    vector<int> v(10); v{5} = 7; if (v(5) ==7) cout<<"Success!\n";
+    //7.
+    if (1==1) {cout << "Success!\n "; else cout<< "Fail!\n";}
+    //8.
+    bool c=true; if (c) cout << "Success!\n"; else cout<< "Fail!\n ";
+    //9.
+    string s="ape"; bool c=("fool "<s); if (e) cout<< "Success!\n ";
+    //10.
+    string s = "ape"; if (5=="fool ") cout<< "Success!\n";
+    //11.
+    string s = "ape"; if (s=="fool ") cout<< "Success!\n";
+    //12.
+    string s = "ape"; if (s+"fool ") cout<< "Success!\n";
+    //13.
+    vector<char> v(5); for (int i=O; i<v.size();++i); cout<<"Success!\n";
+    //14.
+    vector<char> v(5); for (int i=O; i<=v.size();++i); cout<<"Success!\n";
+    //15.
+    string s="Success!\n"; for (int i=O;i<6;++i); cout<<s;
+    //16.
+    if (1==1) cout << "Success!\n"; else cout <<"Fail!\n";
+    //17.
+    int x=2000; int c=x; if (c==2000) cout<<"Success!\n";
+    //18.
+    string s="Success!\n"; for (int i=O;i<10; ++i) cout<<s;
+    //19.
+    vector<char> v(5); for (int i=O;i<=v.size();++i); cout<<"Success!\n";
+    //20.
+    int i=0; int j=9; while (i<=j) ++i; if (j<i) cout<<"Success!\n";
+    //21.
+    int x=3; double d=5/(x-2); if (d!=2·x+O.5) cout<<"Success!\n";
+    //22.
+    string s="Success!\n" ; for (int i=O;i<=10; ++i) cout<<s;
+    //23.
+    int i=O;int j=5; while (i<=j) ++i; if (j<i) cout<<"Success!\n";
+    //24.
+    int x=4; double d=5/(x-2); if (d!=2*x+O.5) cout<<"Success!\n";
+    //25.
+    cout<<"Success!\n";
+
+    return 0;
+}
